4bce31559b82de7b29d37c218b9e27f325af0a28
Sylvain <sylvain.soulayrol@gmail.com>
2021-01-07 11:57:34 +0100

    Clean code

--- README.md
+++ README.md
@@ -1,70 +1,35 @@
-# Getting Started with Create React App
+# Nimby App
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+This app is use for automaticly set the tractor nimby when user use this pc.
+It's use electron (tray icon) and react (main page).
 
-## Available Scripts
+## Dev Doc
 
-In the project directory, you can run:
+This app have script dev, build and deploy.
 
-### `yarn start`
+### Important files
 
-Runs the app in the development mode.\
+- `config.json` in the public folder is the main config with the list of softwares to check if running.
+- `electron.js` is the implementation of the tray via electron and call react index.html
+- `src` folder is react page when you click on the tray icon
+
+### Starting
+
+For start dev, you need node (npm) command install
+Use `npm install` for install all dependencies.
+
+#### Dev
+
+For launch the app in dev mode just launch `npm run dev`.
+If you whant only edit the main page with react use `npm run start`.
 Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
+#### Build
 
-### `yarn test`
+You can build the app with `npm run build`.
+This build electron and react code.
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+#### Deploy
 
-### `yarn build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `yarn eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `yarn build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+When you have working code lauch `npm run deploy` to send build `.exe` to github release.
+You will need to set a github token with repo right. When you have it, use GH_TOKEN environment variable to avoid tocken publish.
--- public/electron.js
+++ public/electron.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, Tray, screen, ipcMain } = require("electron");
+const { app, BrowserWindow, Tray, screen, ipcMain, Notification } = require("electron");
 const { autoUpdater } = require('electron-updater');
 const path = require("path");
 const find = require('find-process');
@@ -19,25 +19,44 @@ const panelSize = {
   visible: false
 }
 
-// Update check 15 min 
-if(!isDev) {
-  setInterval(() => {
-    autoUpdater.checkForUpdates()
-  }, 60000 * 15);
+/*
+******************
+Update Management
+******************
+*/
+if(isDev) {  // Use dummy repo for test
+  autoUpdater.updateConfigPath = path.join(path.dirname(__dirname), 'dev-app-update.yml');
 }
-
+// Update check 15 min
+setInterval(() => {
+  autoUpdater.checkForUpdates()
+}, 60000 * 15);
+// Notification
+autoUpdater.on('update-available', (info) => {
+  const myNotification = new Notification({
+    title: "Nimby App",
+    body: `NimbyApp ${info.version} available.\nThis update will be install automatically`
+  });
+  myNotification.show();
+});
+// Install
 autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
-  autoUpdater.quitAndInstall();
+  setTimeout(() => { autoUpdater.quitAndInstall(); }, 8000);
 });
 autoUpdater.on('error', (error) => {
   console.log(error)
 });
 
-// Nimby
+/*
+******************
+ Nimby Management
+******************
+*/
 var tsid = undefined;
 var hnm = undefined;
 var nimbyOn = false;
 
+// Check process running evry 1 min
 setInterval(() => {
   checkForProcess()
 }, 60000);
@@ -62,6 +81,7 @@ axios.get(`http://localhost:9005/blade/status`)
     console.log("-------------------- ERROR ----------------------------");
   })
 
+// TODO
 if (isDev){
   setInterval(() => {
     axios.get(`http://localhost:9005/blade/ctrl?nimby=0`)
@@ -86,44 +106,65 @@ async function checkForProcess() {
         break;
       }
     }
-  })
+  });
   if(!nimbyOn && processFound) {
-    // Set Nimby on
-    axios.get(`http://localhost:9005/blade/ctrl?nimby=1`)
-      .then(function (response) {
-        console.log("Nimby set ON");
-        nimbyOn = true;
-        // Eject running job
-        if(hnm && tsid) {
-          axios.get(`http://tractor/Tractor/queue?q=ejectall&blade=${hnm}&tsid=${tsid}`)
-            .then(function (response) {
-              console.log("Current jobs ejected");
-            }
-          )
-        }
-      })
-      .catch(function (error) {
-        console.log("-------------------- ERROR ----------------------------");
-        console.log("Error setting nimby ON");
-      }
-    )
+    setNimbyOn()
   }
+  else if (nimbyOn && !processFound) {
+    setNimbyOff()
+  }
+  // Update Tray icon
   if (tray != null) {
     _img = nimbyOn ? path.join(iconDirPath, "artfx_green.png") : path.join(iconDirPath, "artfx_red.png")
     tray.setImage(_img)
   }
 }
 
-// Window
-
-function createPanel(toggleOnLoad = false) {
+function setNimbyOn() {
+  axios.get(`http://localhost:9005/blade/ctrl?nimby=1`)
+    .then(function (response) {
+      console.log("Nimby set ON");
+      console.log(response)
+      nimbyOn = true;
+      if(hnm && tsid) {  // Eject running job
+        axios.get(`http://tractor/Tractor/queue?q=ejectall&blade=${hnm}&tsid=${tsid}`)
+          .then(function (response) {
+            console.log("Current jobs ejected");
+          }
+        )
+      }
+    })
+    .catch(function (error) {
+      console.log("-------------------- ERROR ----------------------------");
+      console.log("Error setting nimby ON");
+    }
+  )
+}
+function setNimbyOff() {
+  axios.get(`http://localhost:9005/blade/ctrl?nimby=0`)
+    .then(function (response) {
+      console.log("Nimby set OFF");
+      console.log(response)
+      nimbyOn = false;
+    })
+    .catch(function (error) {
+      console.log("-------------------- ERROR ----------------------------");
+      console.log("Error setting nimby OFF");
+    }
+  )
+}
+/*
+******************
+ Tray and panel
+******************
+*/
+function createPanel() {
   const { width, height } = screen.getPrimaryDisplay().workAreaSize
   mainWindow = new BrowserWindow({
     width: panelSize.width,
     height: panelSize.height,
     x: width - panelSize.width - 10,
     y: height - panelSize.height - 10,
-    backgroundColor: "#222222",
     frame: false,
     show: false,
     opacity: 0,
@@ -140,26 +181,14 @@ function createPanel(toggleOnLoad = false) {
     // "http://localhost:3000/"
     isDev ? `file://${path.join(__dirname, "index.html")}` : `file://${path.join(__dirname, "../build/index.html")}`
   );
+  // Event
   mainWindow.on("closed", () => (mainWindow = null));
-
   mainWindow.once('ready-to-show', () => {
     mainWindow.show()
-    if (toggleOnLoad) setTimeout(() => { toggleTray(false) }, 33);
-  })
-
+  });
   mainWindow.on("blur", () => {
-    // Only run when not in an overlay
-    mainWindow.webContents.send("panelBlur")
     showPanel(false)
-  })
-
-  mainWindow.on('move', (e) => {
-    e.preventDefault()
-  })
-
-  mainWindow.on('resize', (e) => {
-    e.preventDefault()
-  })
+  });
 }
 
 function createTray() {
@@ -176,9 +205,8 @@ const toggleTray = async () => {
     return false
   }
   if (mainWindow) {
-    showPanel(true, panelSize.height)
+    showPanel(true)
     mainWindow.focus()
-    mainWindow.webContents.send("tray-clicked")
     mainWindow.setSkipTaskbar(false)
     mainWindow.setSkipTaskbar(true)
   }
@@ -195,6 +223,9 @@ function showPanel(show = true) {
 }
 
 app.on("ready", () => {
+  if (!isDev) {
+    autoUpdater.checkForUpdates()
+  }
   createTray()
   createPanel()
   checkForProcess()
